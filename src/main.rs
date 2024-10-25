8c637c8961dd54fca6ec4037b87da95a9434edf4
zhanpengqiu <1219300839@qq.com>
2024-10-25 17:01:00 +0800

    241025_PING+PONG

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,22 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-
-                let mut buf=[0;512];
-                stream.read(&mut buf).unwrap();
-                stream.write(b"+PONG\r\n").unwrap();
-            }
-            Err(e) => {
-                println!("error: {}", e);
+    let mut i=0;
+    while i <3{
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
+                    // let mut buf=[0;512];
+                    // stream.read(&mut buf).unwrap();
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
+        i=i+1;
     }
+
 }
