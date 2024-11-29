d3237363c07296f7359e9f7b7532f6c2b31d6cad
zhanpengqiu <1219300839@qq.com>
2024-11-29 15:25:14 +0800

    asd

--- src/slave_stream.rs
+++ src/slave_stream.rs
@@ -47,7 +47,7 @@ impl Slaves {
 
                         let mut getack_cmd_vec = Vec::new();
                         getack_cmd_vec.push(Value::BulkString(Some("REPLCONF".to_string())));
-                        getack_cmd_vec.push(Value::BulkString(Some("getack".to_string())));
+                        getack_cmd_vec.push(Value::BulkString(Some("GETACK".to_string())));
                         getack_cmd_vec.push(Value::BulkString(Some("*".to_string())));
 
                         handler.write_value(Value::Array(getack_cmd_vec)).await;
