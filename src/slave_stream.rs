6e762f53848c9b497cd89c05b02b63c5ec6f26f1
zhanpengqiu <1219300839@qq.com>
2024-11-29 17:19:01 +0800

    asd

--- src/slave_stream.rs
+++ src/slave_stream.rs
@@ -115,7 +115,7 @@ impl Slaves {
             }
         }
 
-        println!("{:?}", slave_done);
+        // println!("{:?}", slave_done);
         Ok(Value::Integer(slave_done as i64))
         // if self.slave_handler.len() as i32>=slave_num{
         //     Ok(Value::Integer(self.slave_handler.len() as i64))
