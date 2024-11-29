8f6172e64e03209e8e438d93c33e8bd28cf2d62c
zhanpengqiu <1219300839@qq.com>
2024-11-29 18:06:01 +0800

    asd

--- src/db.rs
+++ src/db.rs
@@ -456,17 +456,6 @@ impl RedisDb {
                         res_val = match config_lock.wait(num_of_repl_slaves.clone()).await{
                             Ok(res) => {
                                 res_val = res.clone(); // 假设 res 是你需要的值
-                                match res_val.clone(){
-                                    Value::Integer(v) => {
-                                        // if v <= num_of_repl_slaves.clone() as i64{
-                                        //     continue;
-                                        // }else{
-                                        //     break;
-                                        // }
-                                        continue;
-                                    }
-                                    _=>{continue;}
-                                }
                             }
                             Err(e) => Value::Error(format!("{}",e)),
                         };
