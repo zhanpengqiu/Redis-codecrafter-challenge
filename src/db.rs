2da5b9d61861e1b13acd3d26d38fe904473192ad
zhanpengqiu <1219300839@qq.com>
2024-11-29 17:29:02 +0800

    asd

--- src/db.rs
+++ src/db.rs
@@ -444,7 +444,7 @@ impl RedisDb {
 
 
                 let start_time = Instant::now();
-                let interval = Duration::from_millis(200);
+                let interval = Duration::from_millis(50);
                 let total_duration = Duration::from_millis(wait_num as u64);
                 let mut res_val = Value::BulkString(None);
 
